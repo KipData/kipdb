410335744fd195330310a52e854b4af2a84ee571
Kould <2435992353@qq.com>
2023-06-08 00:08:48 +0800

    refactor(bench): 使用Sled的BenchMark进行标准化基准测试

--- src/bench/kernel_bench.rs
+++ src/bench/kernel_bench.rs
@@ -1,85 +1,215 @@
-use std::sync::atomic::{AtomicU64, Ordering};
+use std::sync::atomic::AtomicU32;
+use std::sync::atomic::Ordering::Relaxed;
 use bytes::Bytes;
-use criterion::{Criterion, criterion_group, criterion_main};
-use tempfile::TempDir;
+/// 参考Sled Benchmark
+/// https://github.com/spacejam/sled/blob/main/benchmarks/criterion/benches/sled.rs
+
+use criterion::{criterion_group, criterion_main, Criterion};
+
 use kip_db::kernel::KVStore;
 use kip_db::kernel::lsm::lsm_kv::LsmStore;
 use kip_db::kernel::sled_kv::SledStore;
-use kip_db::kernel::Result;
 
-/// 持久化内核的简易bench测试
-fn kv_benchmark_with_store<T: KVStore>(c: &mut Criterion) {
+fn counter() -> usize {
+    use std::sync::atomic::AtomicUsize;
+
+    static C: AtomicUsize = AtomicUsize::new(0);
+
+    C.fetch_add(1, Relaxed)
+}
+
+/// Generates a random number in `0..n`.
+fn random(n: u32) -> u32 {
+    use std::cell::Cell;
+    use std::num::Wrapping;
+
+    thread_local! {
+        static RNG: Cell<Wrapping<u32>> = Cell::new(Wrapping(1406868647));
+    }
+
+    RNG.with(|rng| {
+        // This is the 32-bit variant of Xorshift.
+        //
+        // Source: https://en.wikipedia.org/wiki/Xorshift
+        let mut x = rng.get();
+        x ^= x << 13;
+        x ^= x >> 17;
+        x ^= x << 5;
+        rng.set(x);
+
+        // This is a fast alternative to `x % n`.
+        //
+        // Author: Daniel Lemire
+        // Source: https://lemire.me/blog/2016/06/27/a-fast-alternative-to-the-modulo-reduction/
+        ((x.0 as u64).wrapping_mul(n as u64) >> 32) as u32
+    })
+}
+
+fn bulk_load<T: KVStore>(c: &mut Criterion) {
     let rt = tokio::runtime::Builder::new_multi_thread()
         .enable_all()
         .build()
         .unwrap();
 
-    let temp_dir = TempDir::new().expect("unable to create temporary working directory");
+    let count = AtomicU32::new(0_u32);
+    let bytes = |len| -> Vec<u8> {
+        count.fetch_add(1, Relaxed)
+            .to_be_bytes()
+            .into_iter()
+            .cycle()
+            .take(len)
+            .collect()
+    };
 
-    let key1: Vec<u8> = encode_key("key1").unwrap();
-    let key2: Vec<u8> = encode_key("key2").unwrap();
-    let value1: Vec<u8> = encode_key("value1").unwrap();
+    let mut bench = |key_len, val_len| {
+        let db = rt.block_on(async {
+            T::open(format!("bulk_k{}_v{}", key_len, val_len)).await.unwrap()
+        });
 
-    let store = rt.block_on(async {
-        let store = T::open(temp_dir.path()).await.unwrap();
-        // 用于get exist测试获取数据
-        store.set(&key1, Bytes::copy_from_slice(value1.as_slice())).await.unwrap();
-        store
+        c.bench_function(
+            &format!("Store: {}, bulk load key/value lengths {}/{}", T::name(), key_len, val_len),
+            |b| {
+                b.to_async(&rt).iter(|| async {
+                    db.set(
+                        &bytes(key_len),
+                        Bytes::from(bytes(val_len))
+                    ).await.unwrap();
+                })
+            },
+        );
+    };
+
+    for key_len in &[10_usize, 128, 256, 512] {
+        for val_len in &[0_usize, 10, 128, 256, 512, 1024, 2048, 4096, 8192] {
+            bench(*key_len, *val_len);
+        }
+    }
+}
+
+fn monotonic_crud<T: KVStore>(c: &mut Criterion) {
+    let rt = tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()
+        .unwrap();
+    rt.block_on(async {
+        let db = T::open("monotonic_crud").await.unwrap();
+
+        c.bench_function(
+            &format!("Store: {}, monotonic inserts", T::name()),
+            |b| {
+
+                let count = AtomicU32::new(0_u32);
+                b.iter(|| async {
+                    db.set(&count.fetch_add(1, Relaxed).to_be_bytes(), Bytes::new()).await.unwrap();
+                })
+            }
+        );
+
+        c.bench_function(
+            &format!("Store: {}, monotonic gets", T::name()),
+            |b| {
+                let count = AtomicU32::new(0_u32);
+                b.iter(|| async {
+                    db.get(&count.fetch_add(1, Relaxed).to_be_bytes()).await.unwrap();
+                })
+            }
+        );
+
+        c.bench_function(
+            &format!("Store: {}, monotonic removals", T::name()),
+
+            |b| {
+                let count = AtomicU32::new(0_u32);
+                b.iter(|| async {
+                    db.remove(&count.fetch_add(1, Relaxed).to_be_bytes()).await.unwrap();
+                })
+            }
+        );
     });
+}
 
-    c.bench_function(&store_name_with_test::<T>("get exist"), |b| {
-        b.to_async(&rt).iter(|| {
-            async {
-                store.get(&key1).await
-                    .unwrap()
+fn random_crud<T: KVStore>(c: &mut Criterion) {
+    const SIZE: u32 = 65536;
+
+    let rt = tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()
+        .unwrap();
+    rt.block_on(async {
+        let db = T::open("random_crud").await.unwrap();
+
+        c.bench_function(
+            &format!("Store: {}, random inserts", T::name()),
+            |b| {
+                b.iter(|| async {
+                    db.set(&random(SIZE).to_be_bytes(), Bytes::new()).await.unwrap();
+                })
             }
-        })
+        );
+
+        c.bench_function(
+            &format!("Store: {}, random gets", T::name()),
+            |b| {
+                b.iter(|| async {
+                    db.get(&random(SIZE).to_be_bytes()).await.unwrap();
+                })
+            }
+        );
+
+        c.bench_function(
+            &format!("Store: {}, random removals", T::name()),
+            |b| {
+                b.iter(|| async {
+                    db.remove(&random(SIZE).to_be_bytes()).await.unwrap();
+                })
+            }
+        );
     });
-
-    c.bench_function(&store_name_with_test::<T>("get not exist"), |b|
-        b.to_async(&rt).iter(|| {
-            async {
-                store.get(&key2).await
-                    .unwrap()
-            }
-        }));
-
-    // 使用count进行动态的数据名变更防止数据重复而导致不写入
-    let seq_buf_set = AtomicU64::new(0);
-    let value = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ
-                            abcdefghijklmnopqrstuvwxyz
-                            0123456789)(*&^%$#@!~".to_vec();
-    c.bench_function(&store_name_with_test::<T>("set value"), |b|
-        b.to_async(&rt).iter(|| {
-            async {
-                store.set(&seq_buf_set.fetch_add(1, Ordering::SeqCst).to_be_bytes(), Bytes::copy_from_slice(value.as_slice())).await
-                    .unwrap();
-            }
-        }));
-
-    let seq_buf_remove = AtomicU64::new(0);
-    c.bench_function(&store_name_with_test::<T>("remove not exist value"), |b|
-        b.to_async(&rt).iter(|| {
-            async {
-                let _ = store.remove(&seq_buf_remove.fetch_add(1, Ordering::SeqCst).to_be_bytes()).await;
-            }
-        }));
 }
 
-fn kv_benchmark(c: &mut Criterion) {
-    kv_benchmark_with_store::<LsmStore>(c);
-    kv_benchmark_with_store::<SledStore>(c);
+fn empty_opens<T: KVStore>(c: &mut Criterion) {
+    let rt = tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()
+        .unwrap();
+
+    let _ = std::fs::remove_dir_all("empty_opens");
+    c.bench_function(
+        &format!("Store: {}, empty opens", T::name()),
+        |b| {
+            b.to_async(&rt).iter(|| async {
+                let _ = T::open(format!("empty_opens/{}.db", counter())).await.unwrap();
+            })
+        }
+    );
+    let _ = std::fs::remove_dir_all("empty_opens");
 }
 
-fn store_name_with_test<T: KVStore>(test_name :& str) -> String {
-    format!("{}: {}",T::name(), test_name)
+fn kv_bulk_load(c: &mut Criterion) {
+    bulk_load::<LsmStore>(c);
+    bulk_load::<SledStore>(c);
 }
 
-criterion_group!(benches, kv_benchmark);
+fn kv_monotonic_crud(c: &mut Criterion) {
+    monotonic_crud::<LsmStore>(c);
+    monotonic_crud::<SledStore>(c);
+}
+
+fn kv_random_crud(c: &mut Criterion) {
+    random_crud::<LsmStore>(c);
+    random_crud::<SledStore>(c);
+}
+
+fn kv_empty_opens(c: &mut Criterion) {
+    empty_opens::<LsmStore>(c);
+    empty_opens::<SledStore>(c);
+}
+
+criterion_group!(
+    benches,
+    kv_bulk_load,
+    kv_monotonic_crud,
+    kv_random_crud,
+    kv_empty_opens
+);
 criterion_main!(benches);
-
-// 测试用序列化方法
-fn encode_key(key: &str) -> Result<Vec<u8>>{
-    Ok(bincode::serialize(key)?)
-}
-
